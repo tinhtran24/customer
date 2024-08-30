e61be1716571b745c367b01bf9e7dd908ccd3b1a
Tinh Tran <56858011+tinhtran24@users.noreply.github.com>
2024-08-30 04:56:08 +0000

    feat: space

--- customer-manager-frontend/src/app/utils/generateString.ts
+++ customer-manager-frontend/src/app/utils/generateString.ts
@@ -1,4 +1,4 @@
-export function generateCode (label:string,date: Date, sequence: number): string {
+export function generateCode (label:string, date: Date, sequence: number): string {
     const year = date.getFullYear();
     return `${label}_${year}${sequence.toString().padStart(8, '0')}`;
 }
