c2e474f2ba2aea086bc8c81ba9831d501287d173
Tinh Tran <56858011+tinhtran24@users.noreply.github.com>
2024-09-19 04:17:25 +0000

    feat: deploy script

--- customer-manager-frontend/ecosystem.config.js
+++ customer-manager-frontend/ecosystem.config.js
@@ -1,22 +1,11 @@
 module.exports = {
     apps: [
-        {
-            script: "npm run start",
-        },
-    ],
-
-    deploy: {
-        production: {
-            user: "root",
-            host: "125.212.231.98",
-            ref: "origin/main",
-            repo: "https://github.com/tinhtran24/customer",
-            path: "/home/ubuntu/",
-            "pre-deploy-local": "",
-            "post-deploy":
-                "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
-            "pre-setup": "",
-            "ssh-options": "ForwardAgent=yes",
-        },
-    },
-};
+      {
+        name: 'CustomerFE',
+        exec_mode: 'cluster',
+        instances: 'max', // Or a number of instances
+        script: 'node_modules/next/dist/bin/next',
+        args: 'start'
+      }
+    ]
+  }
