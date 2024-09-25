2e6085c0740ed108ce413c32b4cf714e1a0f5c88
Max Tran <max_tran@nfcguest.com>
2024-09-25 09:22:38 +0700

    feat: increases instance

--- customer-manager-backend/ecosystem.config.js
+++ customer-manager-backend/ecosystem.config.js
@@ -2,9 +2,9 @@ module.exports = {
     apps: [{
         name: 'customer-api',
         script: './dist/main.js',
-
         // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-        instances: 1,
+        exec_mode: 'cluster',
+        instances: 3,
         autorestart: true,
         watch: false,
         max_memory_restart: '1G',
